a384cfed7c6bc9e10d4d557df3c956f799577a02
Unknown <vipermuffin@gmail.com>
2020-12-01 09:48:39 -0600

    Day 01 Solutions

---
+++ Day01/Day01.h
@@ -0,0 +1,16 @@
+//
+//  Advent of Code 2020 Solutions: Day 1
+//
+//  https://adventofcode.com/2020
+//
+//  Created by vipermuffin on 11/30/2020.
+//  Copyright © 2020 vipermuffin. All rights reserved.
+//
+#pragma once
+#include <string>
+#include <vector>
+
+namespace AocDay01 {
+//Function Definitions
+
+}
